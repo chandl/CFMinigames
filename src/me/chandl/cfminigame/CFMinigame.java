76c68475da3de6316d8a44e0f2dfb714863097e1
Chandler Severson <me@cseverson.com>
2017-07-30 18:44:49 -0700

    remove database connector

--- src/me/chandl/cfminigame/CFMinigame.java
+++ src/me/chandl/cfminigame/CFMinigame.java
@@ -1,16 +1,13 @@
 package me.chandl.cfminigame;
 
 
-import me.chandl.cfminigame.database.DatabaseConnector;
 import me.chandl.cfminigame.minigame.Minigame;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.sql.SQLException;
 import java.util.logging.Logger;
 
 public class CFMinigame extends JavaPlugin{
 
-    private DatabaseConnector connector;
     private Logger log;
     private static Minigame currentMinigame;
 
@@ -19,17 +16,6 @@ public class CFMinigame extends JavaPlugin{
         log = getLogger();
         log.info("onEnable called.");
 
-
-        //Connect to database
-        connector =  DatabaseConnector.getConnector("127.0.0.1", "cfMinigame", "root", "$tream1T", 3306);
-        try {
-            log.info("Opening Database Connection.");
-            connector.openConnection();
-        } catch (SQLException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
 
         //Handle Commands
         this.getCommand("mg").setExecutor(new CommandHandler());
