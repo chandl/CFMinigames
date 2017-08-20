9a2878c741a59614879f009eedfb343d01da0e7c
Chandler Severson <me@cseverson.com>
2017-08-19 22:45:36 -0700

    Add documentation to CFMinigame.java

--- src/me/chandl/cfminigame/CFMinigame.java
+++ src/me/chandl/cfminigame/CFMinigame.java
@@ -14,9 +14,22 @@ import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
+/**
+ * CFMinigame - A Minecraft Minigame System
+ *
+ *
+ * @author Chandler me@cseverson.com
+ * @version 1.0
+ * @since Aug 19, 2017
+ *
+ */
 public class CFMinigame extends JavaPlugin{
+
+    //Constants for the Minigame System.
     public static final int DEFAULT_MAX_PLAYERS = 16;
     public static final int DEFAULT_MIN_PLAYERS = 2;
+
+    //Difficulty Levels
     public static final int DEFAULT_MIN_DIFFICULTY = 1;
     public static final int DEFAULT_MAX_DIFFICULTY = 5;
 
@@ -42,29 +55,25 @@ public class CFMinigame extends JavaPlugin{
         ConfigurationSerialization.registerClass(Checkpoint.class);
     }
 
+    /**
+     * Registers a Custom Listener with the Plugin Manager.
+     * Usually called by a Minigame in its {@code start()} method.
+     *
+     * @param listener the {@link MinigameListener} that is used in conjunction with a specific {@link me.chandl.cfminigame.minigame.core.MinigameType}.
+     */
     public void registerListener(MinigameListener listener){
         getServer().getPluginManager().registerEvents(listener, this);
     }
 
+    /**
+     * Unregisters a Custom Listener.
+     * Usually called by a Minigame in its {@code stop()} method.
+     *
+     * @param listener the {@link MinigameListener} that is used in conjunction with a specific {@link me.chandl.cfminigame.minigame.core.MinigameType}
+     */
     public void unregisterListener(MinigameListener listener){
         HandlerList.unregisterAll(listener);
     }
-
-//    public void registerRaceHandler(){
-//        getServer().getPluginManager().registerEvents(RaceListener.getListener(), this);
-//    }
-//
-//    public void unregisterRaceHandler(){
-//        HandlerList.unregisterAll(RaceListener.getListener());
-//    }
-//
-//    public void registerSnowballHandler(){
-//        getServer().getPluginManager().registerEvents(SnowballListener.getListener(), this);
-//    }
-//
-//    public void unregisterSnowballHandler(){
-//        HandlerList.unregisterAll(SnowballListener.getListener());
-//    }
 
 
     @Override
