e4b7dfd06d598405922949ab78a8208a175e466f
Chandler Severson <me@cseverson.com>
2017-08-19 23:14:44 -0700

    Add custom Exceptions for new MinigameFactory

---
+++ src/me/chandl/cfminigame/handler/PlayerHandler.java
@@ -0,0 +1,7 @@
+package me.chandl.cfminigame.handler;
+
+/**
+ * Created by chandler on 8/19/17.
+ */
+public class PlayerHandler {
+}
---
+++ src/me/chandl/cfminigame/minigame/MinigameFactory.java
@@ -0,0 +1,7 @@
+package me.chandl.cfminigame.minigame;
+
+/**
+ * Created by chandler on 8/19/17.
+ */
+public class MinigameFactory {
+}
